3de3851af6591ff52205aca010cc56377b716493
Stefanie Cortina <stefanie_0627@hotmail.com>
2021-10-09 11:12:59 -0500

    dash

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('login.html')
 
-@app.route('/dashboard.html')
+@app.route('/dash')
 def dashboard():
     return render_template('dashboard.html')
 
-@app.route('/register.html')
+@app.route('/register')
 def register():
     return render_template('register.html')
     
-@app.route('/editarEmpleado.html')
+@app.route('/editar')
 def editar():
     return render_template('editarEmpleado.html')
 
