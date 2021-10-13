c6fa05510b45d82b50ac296ebcacc9ca7c231630
johand2021-x <90793798+GRUPO429Cilco3@users.noreply.github.com>
2021-10-13 18:52:50 -0500

    .0

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf.form import FlaskForm
-from wtforms import StringField,PasswordField,BooleanField,SubmitField
+from wtforms import StringField,PasswordField,BooleanField,SubmitField,SelectField,DecimalField,DateField
 from wtforms.validators import DataRequired, Email, length
 
 
@@ -11,6 +11,17 @@ class LoginForm(FlaskForm):
 
 class SignupForm(FlaskForm):
     name = StringField('Nombre', validators=[DataRequired(), length(max=64)])
+    lastname = StringField('Apellido', validators=[DataRequired(), length(max=64)])
+    address = StringField('Direccion')
+    gender = SelectField('Sexo', choices=['M','F'])
+    employee_id = StringField('Numero de Identificacion')
+    branch = SelectField('Dependencia')
+    job_title = SelectField('Cargo')
+    contract = StringField('Num. de Contrato')
+    salary = DecimalField('Salario')
+    contract_start = DateField('Fecha Inicio Contrato')
+    contract_end = DateField('Fecha Inicio Contrato')
     password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Password confirmation', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Registrar')
